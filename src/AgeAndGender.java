f41fecef455d4248932fbf9a38699f63a77627e4
Mikhail Volzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2024-04-06 12:27:46 +0300

    added help class and logic

--- src/AgeAndGender.java
+++ src/AgeAndGender.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class AgeAndGender {
+import java.time.LocalDate;
+import java.time.Period;
+
+public class AgeAndGender {
+    public char determineGender(String patronymic) {
+        char lastChar = patronymic.charAt(patronymic.length() - 1);
+        return (lastChar == 'ч' || lastChar == 'Ш') ? 'М' : 'Ж';
+    }
+
+    public int calculateAge(LocalDate birthDate) {
+        LocalDate currentDate = LocalDate.now();
+        return Period.between(birthDate, currentDate).getYears();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        AgeAndGender help = new AgeAndGender();
+
+        System.out.println("Введите Фамилию:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Введите Имя:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Введите Отчество:");
+        String patronymic = scanner.nextLine();
+
+        System.out.println("Введите дату рождения в формате ГГГГ-ММ-ДД:");
+        String birthDateStr = scanner.nextLine();
+        LocalDate birthDate = LocalDate.parse(birthDateStr);
+
+        char gender = help.determineGender(patronymic);
+
+        int age = help.calculateAge(birthDate);
+
+        System.out.println("Фамилия и инициалы: " + lastName + " " + firstName.charAt(0) + "." + patronymic.charAt(0) + ".");
+        System.out.println("Пол: " + (gender == 'М' ? "Мужской" : "Женский"));
+        System.out.println("Возраст: " + age + " лет");
+
+        scanner.close();
     }
 }
